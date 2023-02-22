0a0ebd0a0473727c304c27c09411530178017abb
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-02-22 23:37:13 +0000

    Solution - all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  outputString = '';
+  
+  // Check if input num is negative. In this case, it should stop and retrieve error, according to test 5!
+  if (num < 0) {
+    return 'ERROR';
+  }
 
+  for (let i = 0; i < num; i++) {
+    outputString += string;
+  }
+  return outputString;
 };
 
 // Do not edit below this line
