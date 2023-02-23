71c1f14392b29237dd4cc41baf00a23f19097ec4
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-02-23 00:58:15 +0000

    Solution - array of arguments and nested for loop with array.splice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+  // convert exclusions into an array of different items
+  const exclusionList = [];
+  for (i = 1; i < arguments.length; i++) {
+    exclusionList.push(arguments[i]);
+  }
 
+  for (let a = array.length - 1; a >= 0; a--) {
+    for (let eL = 0; eL < exclusionList.length; eL++) {
+      if (array[a] === exclusionList[eL]) {
+        array.splice(a, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
