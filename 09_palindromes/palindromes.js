7cbc4014b5041c838a00cfadcb4458fcb65dec50
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-02-23 22:07:23 +0000

    Solution, conversion string + check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,36 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let adjustedString = '';
+  for (i = 0; i < string.length; i++) {
+    if (string.charCodeAt(i) > 59 && string.charCodeAt(i) < 91) {
+      adjustedString += string[i];
+    } else if (string.charCodeAt(i) > 96 && string.charCodeAt(i) < 123) {
+      adjustedString += string[i].toUpperCase();
+    }
+  }
 
+  let check = false;
+
+  if (adjustedString.length % 2 === 0) {
+    for (let i = 0; i < adjustedString.length / 2; i++) {
+      if (adjustedString[i] === adjustedString[(adjustedString.length - 1) - i]) {
+        check = true;
+      } else {
+        check = false;
+        break;
+      }
+    }
+  } else {
+    for (let i = 0; i < Math.floor(adjustedString.length / 2); i++) {
+      if (adjustedString[i] === adjustedString[(adjustedString.length - 1) - i]) {
+        check = true;
+      } else {
+        check = false;
+        break;
+      }
+    }
+  }
+
+  return check;
 };
 
 // Do not edit below this line
