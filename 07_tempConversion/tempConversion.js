fbaf78090a5b0f53520b13b1ba2c253af032b80f
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-02-23 16:00:27 +0000

    Solution - error + parseFloat(result.toFixed(1))

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (typeof temp !== 'number') {
+    return 'ERROR';
+  }
+
+  let celsius;
+
+  celsius = (temp - 32) * 5 / 9;
+
+  // .toFixed defines the number of decimals but converts the result to a string, parseFloat() converts it back to a number
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (typeof temp !== 'number') {
+    return 'ERROR';
+  }
+
+  let fahrenheit;
+
+  fahrenheit = (temp * 9 / 5 + 32);
+
+  // .toFixed defines the number of decimals but converts the result to a string, parseFloat() converts it back to a number
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
