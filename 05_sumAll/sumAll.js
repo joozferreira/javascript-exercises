c07507e4f33432b2cab9877a74212f368b540ae5
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-02-23 15:12:30 +0000

    Solution - all tests passed, args to start/end, error handling, result

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(...Args) {
+  let result = 0;
+  let start;
+  let end;
 
+  // Definition of beginning and end to avoid use of i-- on the for loop
+  if (Args[0] < Args[1]) {
+    start = Args[0];
+    end = Args[1];
+  } else {
+    start = Args[1];
+    end = Args[0];
+  }
+
+  // Detection of errors: If negative numbers and if input are not of type number
+  if (typeof Args[0] !== 'number' || typeof Args[1] !== 'number' || Args[0] < 0 || Args[1] < 0) {
+    return 'ERROR';
+  }
+
+  for (let i = start; i < end + 1; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
