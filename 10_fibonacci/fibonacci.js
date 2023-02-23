7b6e686f454df7eb43692fb80f56966f0785756b
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-02-23 23:40:04 +0000

    Solution, check neg numbers & conversion of strings

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(item) {
+  if (parseInt(item) < 1) {
+    return 'OOPS';
+  }
 
+  if (parseInt(item) === 1 || parseInt(item) === 2) {
+    return 1;
+  } else {
+    return fibonacci(parseInt(item) - 1) + fibonacci(parseInt(item) - 2);
+  }
 };
 
 // Do not edit below this line
