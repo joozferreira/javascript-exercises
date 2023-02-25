8af4b42f4e4a2e3c8d51a00fa6b1f769e643a592
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-02-25 21:18:07 +0000

    Solution, all tests passed, to be refactored

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const age = [];
+  let maxAgePosition = 0;
 
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].yearOfDeath === undefined) {
+      age.push(new Date().getFullYear() - people[i].yearOfBirth);
+    } else {
+      age.push(people[i].yearOfDeath - people[i].yearOfBirth);
+    }
+  }
+
+  for (let i = 0; i < age.length; i++) {
+    if (age[i] > age[maxAgePosition]) {
+      maxAgePosition = i;
+    }
+  }
+
+  return people[maxAgePosition];
 };
 
 // Do not edit below this line
