ebc31528463efb41a43f18fcd35f16277e4299c6
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-02-26 13:38:42 +0000

    Commenting

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,12 +1,16 @@
 const findTheOldest = function(people) {
+  // creation of an array with the age of each of the elements of array people
   const age = people.map((item) => {
     return item.yearOfDeath
-    ? item.yearOfDeath - item.yearOfBirth
-    : new Date().getFullYear() - item.yearOfBirth;
+      ? item.yearOfDeath - item.yearOfBirth
+      : new Date().getFullYear() - item.yearOfBirth;
   });
 
+  // Math.max(...age) will retrieve the max within the age array
+  // .indexOf gives the position of that max age
   let maxAgePosition = age.indexOf(Math.max(...age));
 
+  // We can now return the person for the index with highest age
   return people[maxAgePosition];
 };
 
