e64b6a35bb5a464c0d824d918469762802a2e9ec
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-12-02 23:57:44 +0000

    Refactor of code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,36 +1,14 @@
 const palindromes = function (string) {
   let adjustedString = '';
   for (i = 0; i < string.length; i++) {
-    if (string.charCodeAt(i) > 59 && string.charCodeAt(i) < 91) {
-      adjustedString += string[i];
-    } else if (string.charCodeAt(i) > 96 && string.charCodeAt(i) < 123) {
+    if (string.toUpperCase().charCodeAt(i) > 59 && string.toUpperCase().charCodeAt(i) < 91) {
       adjustedString += string[i].toUpperCase();
     }
   }
 
-  let check = false;
+  let reversedAdjusted = adjustedString.split("").reverse().join("");
 
-  if (adjustedString.length % 2 === 0) {
-    for (let i = 0; i < adjustedString.length / 2; i++) {
-      if (adjustedString[i] === adjustedString[(adjustedString.length - 1) - i]) {
-        check = true;
-      } else {
-        check = false;
-        break;
-      }
-    }
-  } else {
-    for (let i = 0; i < Math.floor(adjustedString.length / 2); i++) {
-      if (adjustedString[i] === adjustedString[(adjustedString.length - 1) - i]) {
-        check = true;
-      } else {
-        check = false;
-        break;
-      }
-    }
-  }
-
-  return check;
+  return (adjustedString === reversedAdjusted) ? true : false;
 };
 
 // Do not edit below this line
