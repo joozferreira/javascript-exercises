be031ee880be7cb69bb9331069bb784e6ca1c2a2
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-12-03 00:12:11 +0000

    Add case for Fibonacci(0)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,8 @@
 const fibonacci = function(item) {
   const position = parseInt(item);
 
-  return position < 1 ? "OOPS" : 
+  return position < 0 ? "OOPS" : // not possible to have fibonacci of negative numbers
+        position < 1 ? 0 : // Fibonacci(0) should be 0
         position < 3 ? 1 : // Covers the case of element 1 and 2 which should be 1
         fibonacci(position - 1) + fibonacci(position - 2);
 };
